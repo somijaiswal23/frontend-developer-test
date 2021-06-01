72643926095eb94c2aa440c87e2d856b0a8dc402
somijaiswal23 <somi.jaiswal@iiitb.net>
2021-06-01 20:54:11 +1000

    Removed console

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,5 @@ export const isLoggedIn =()=>{
     return window.localStorage.getItem('AUTH_TOKEN')
 }
 export const logout =()=>{
-    console.log("logout")
     return window.localStorage.removeItem('AUTH_TOKEN')
 }
